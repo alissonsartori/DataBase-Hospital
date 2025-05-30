a4ebd5851cf7f9ce339ddf65fad4b59afeef3c98
Alisson Sebastian Sartori <113254957+alissonsartori@users.noreply.github.com>
2025-05-30 09:49:23 -0300

    Update PARTE 4 - Alterando o banco de dados.sql

--- PARTE 4 - Alterando o banco de dados.sql
+++ PARTE 4 - Alterando o banco de dados.sql
@@ -1,14 +1,16 @@
+SET SQL_SAFE_UPDATES = 0; -- Disable safe update mode for the current session
+
 -- Adicionar a coluna 'em_atividade' à tabela medicos
 ALTER TABLE medicos
 ADD COLUMN em_atividade BOOLEAN DEFAULT TRUE;
 
----
-
 -- Atualizar médicos como inativos e ativos
 UPDATE medicos
 SET em_atividade = FALSE
-WHERE nome IN ('Dra. Ana Costa', 'Dra. Gabriela Lima'); -- Dois médicos inativos
+WHERE nome IN ('Dra. Ana Costa', 'Dra. Gabriela Lima');
 
 UPDATE medicos
 SET em_atividade = TRUE
-WHERE nome NOT IN ('Dra. Ana Costa', 'Dra. Gabriela Lima'); -- Os demais em atividade
+WHERE nome NOT IN ('Dra. Ana Costa', 'Dra. Gabriela Lima');
+
+SET SQL_SAFE_UPDATES = 1; -- Re-enable safe update mode
